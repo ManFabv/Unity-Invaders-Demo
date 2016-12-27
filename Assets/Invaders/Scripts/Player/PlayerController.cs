22f13f057b5ef19d0589d94d04f63e62cf52a616
Fabricio Manrique <fabemanrique@hotmail.com>
2016-12-26 21:52:58 -0300

    Setup escena basica de juego con el fondo y el player (solo movimiento lateral) agregados.

---
+++ Assets/Invaders/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float moveVelocity = 5;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public float Velocity
+    {
+        get; private set;
+    }
+	
+	void Update ()
+    {
+        Velocity = Input.GetAxis("Horizontal") * moveVelocity;
+	}
+}
---
+++ Assets/Invaders/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    private PlayerController playerMotor;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        //Esto se que siempre se cumplira porque requiero este componente
+        //en la declaracion de la clase
+        playerMotor = this.GetComponent<PlayerController>();
+        rb = this.GetComponent<Rigidbody>();   
+    }
+
+    /// <summary>
+    /// Metodo llamado a una cantidad fija de tiempo (manteniendo la cantidad de llamadas por segundo)
+    /// </summary>
+	void FixedUpdate ()
+    {
+        rb.AddForce(new Vector3(playerMotor.Velocity, 0, 0) * Time.deltaTime, ForceMode.Impulse);
+	}
+}
