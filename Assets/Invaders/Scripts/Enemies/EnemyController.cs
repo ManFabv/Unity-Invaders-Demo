c11131874d2193bb5a7a98bcb4f0cc2107f781d4
Fabricio Manrique <fabemanrique@hotmail.com>
2017-01-30 14:18:54 -0300

    Agregadas las animaciones basicas de los enemigos. Falta que se muevan y disparen

---
+++ Assets/Invaders/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(ParticleSystem))]
+public class EnemyController : MonoBehaviour {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private Animator anim;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private ParticleSystem particles;
+
+    private void Awake()
+    {
+        anim = this.GetComponent<Animator>();
+
+        particles = this.GetComponent<ParticleSystem>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(Literals.tagPlayerShot))
+        {
+            particles.Play();
+            anim.SetBool(Literals.animatorNameToStartEnemyDeath, true);
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    void Death()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
--- Assets/Invaders/Scripts/Helpers/Literals.cs
+++ Assets/Invaders/Scripts/Helpers/Literals.cs
@@ -3,32 +3,37 @@
     protected Literals() { } //para que no pueda ser instanciada la clase
 
     /// <summary>
-    /// Referencia al layer de las armas de los enemigos
+    /// 
+    /// </summary>
+    public static readonly string tagEnemy = "Enemy";
+
+    /// <summary>
+    /// 
     /// </summary>
     public static readonly string tagEnemyShot = "EnemyShot";
 
     /// <summary>
-    /// Referencia al layer de las armas del jugador
+    /// 
     /// </summary>
     public static readonly string tagPlayerShot = "PlayerShot";
 
     /// <summary>
-    /// Referencia al layer de las armas del jugador
+    /// 
     /// </summary>
     public static readonly string horizontalAxis = "Horizontal";
 
     /// <summary>
-    /// Referencia al layer de las armas del jugador
+    /// 
     /// </summary>
     public static readonly string fireButton = "Fire1";
 
     /// <summary>
-    /// Referencia al layer de las armas del jugador
+    /// 
     /// </summary>
     public static readonly string layerNamePlayerWeapon = "PlayerShot";
 
     /// <summary>
-    /// Referencia al layer del jugador
+    /// 
     /// </summary>
     public static readonly string layerNamePlayer = "Player";
 
@@ -36,4 +41,9 @@
     /// 
     /// </summary>
     public static readonly float timeToInitPlayerInput = 1;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static readonly string animatorNameToStartEnemyDeath = "Death";
 }
